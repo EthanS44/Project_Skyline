02916449e7e7715bf96d869bc4ce1d9bfe95d12a
Sam <samwilson292929@gmail.com>
2025-02-06 16:52:21 -0500

    Changes by ethanstewart

--- src/main/java/org/Skyline/DatabaseManager.java
+++ src/main/java/org/Skyline/DatabaseManager.java
@@ -3,12 +3,27 @@ package org.Skyline;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
+import java.sql.*;
+
+
 @Controller
 public class DatabaseManager {
-    @Autowired
-    private ModelRepository modelRepository;
+
+    private static final String url = "postgresql://viaduct.proxy.rlwy.net:17205/railway";
+    private static final String user = "postgres";
+    private static final String password = "SqyAeJuIfQNzpFFxyxczlwTlrcYCwUEc";
+
 
     public void saveModel(Model model){
-        modelRepository.save(model);
+        // save model
+        try{
+            Connection connection = DriverManager.getConnection(url, user, password);
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
+
+    public saveUser()
 }
